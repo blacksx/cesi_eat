5392007031dfa8d819d272bf7c39f9a2367865f4
Exochasse <38660196+Exochasse@users.noreply.github.com>
2021-06-29 20:01:59 +0200

    test

---
+++ backend/connector/connectorMongoDB.js
@@ -0,0 +1,8 @@
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://admin:admin@cluster0.g8zvu.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
